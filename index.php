112b09bc7ef2bc00e7c7351ffb31c5137066c33b
setter43 <49942725+setter43@users.noreply.github.com>
2023-01-18 09:59:50 +0000

    Add files via upload
    Added comments and embedded PHP into HTML

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
+    //Creating Vars in PHP
     $firstName = 'John';
-
-    echo 'Hello, ' . $firstName . '!';
-
+    $lastName = 'Doe';
+    $age = 30;
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,6 +17,11 @@
 <body>
     
 <h1>User Profile Page</h1>
+<div>
+    <!-- Reads from PHP and Displays-->
+    <p><?php echo $firstName . ' ' . $lastName; ?></p>
+    <p><?php echo $age; ?></p>
+</div>
 
 </body>
 </html>
